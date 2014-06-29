c235b6a13e267d1a6bc7fd269855eb9e24e636c2
Ty Overby <ty@pre-alpha.com>
2014-06-29 15:19:14 -0700

    chain together install scripts for early failure

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-./install-glfw.sh
-./install-portaudio.sh
-./install-sdl2.sh
-./install-sdl2-ttf.sh
+./install-glfw.sh && \
+./install-portaudio.sh && \
+./install-sdl2.sh && \
+./install-sdl2-ttf.sh && \
 ./install-sdl-mixer.sh
