99fd7b3963a124b866ef0900250f8cbc6e54c0bf
Ty Overby <ty@pre-alpha.com>
2014-06-29 21:28:18 -0700

    added gates for features

--- install.sh
+++ install.sh
@@ -1,37 +1,36 @@
 #!/bin/bash
 
-echo "$DEPS_DIR" >> test_out.txt
-echo "$OUT_DIR" >> test_out.txt
-
 # GLFW
-if [ ! -f "./lib/libglfw.so" ]; then
+if [ $GLFW ] && [ ! -f "./lib/libglfw.so" ]; then
     echo "installing GLFW"
     ./install-glfw.sh
 fi
 
 # PORTAUDIO
-if [ ! -f "./lib/libportaudio.so" ]; then
+if [ $PORTAUDIO ] && [ ! -f "./lib/libportaudio.so" ]; then
     echo "installing portaudio"
     ./install-portaudio.sh
 fi
 
 # SDL2
-if [ ! -f "./lib/libSDL2-2.0.so.0" ]; then
+if [ $SDL2 ] && [ ! -f "./lib/libSDL2-2.0.so.0" ]; then
     echo "installing SDL2"
     ./install-sdl2.sh
 fi
 
 # SDL2-TTF
-if [ ! -f "./lib/libSDL2_ttf.so" ]; then
+if [ $SDL2_TTF ] && [ ! -f "./lib/libSDL2_ttf.so" ]; then
     echo "installing sdl2-ttf"
     ./install-sdl2-ttf.sh
 fi
 
 # SDL-MIXER
-if [ ! -f "./lib/libSDL2_mixer.so" ]; then
+if [ $SDL_MIXER ] && [ ! -f "./lib/libSDL2_mixer.so" ]; then
     echo "installing sdl-mixer"
     ./install-sdl-mixer.sh
 fi
 
 # $OUT_DIR is provided to us by cargo
-cp -r lib/* $OUT_DIR
+if [ -d ./lib/ ]; then
+    cp -r lib/* $OUT_DIR
+fi
